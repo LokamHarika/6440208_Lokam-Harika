0cfbefefe0765114846484760eae6f55675e2d90
LokamHarika <harikalokam2005@gmail.com>
2025-05-28 11:50:48 +0530

    Update FactorialCalculator.java

--- Module-3/FactorialCalculator.java
+++ Module-3/FactorialCalculator.java
@@ -4,16 +4,15 @@ public class FactorialCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a non-negative integer: ");
-        int num = sc.nextInt();
-
-        if (num < 0) {
+        int n = sc.nextInt();
+        if (n < 0) {
             System.out.println("Invalid input. Please enter a non-negative integer.");
         } else {
-            long factorial = 1;
-            for (int i = 1; i <= num; i++) {
-                factorial *= i;
+            long fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact *= i;
             }
-            System.out.println("Factorial of " + num + " is: " + factorial);
+            System.out.println("Factorial of " + n + " is: " + fact);
         }
         sc.close();
     }
