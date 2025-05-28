00838d490a758aa4deb893bb23754c707bc51789
LokamHarika <harikalokam2005@gmail.com>
2025-05-28 11:54:03 +0530

    Update MultiplicationTable.java

--- Module-3/MultiplicationTable.java
+++ Module-3/MultiplicationTable.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter a number: ");
-        int num = scanner.nextInt();
-
-        System.out.println("Multiplication table for " + num + ":");
+        int n = scanner.nextInt();
+        System.out.println("Multiplication table for " + n + ":");
         for (int i = 1; i <= 10; i++) {
-            System.out.println(num + " x " + i + " = " + (num * i));
+            System.out.println(n + " x " + i + " = " + (n * i));
         }
-
         scanner.close();
     }
 }
